038cbbaf5632274315964730a40710b628cc6f5a
weihuijie <huijie_wei@163.com>
2019-05-09 13:59:39 +0800

    创建项目。

---
+++ src/test/java/com/x/book/ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.x.book;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyBookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
